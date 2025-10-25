4173ccaf32e2049212e6761b06da2c7c9ae4580f
Chetan <chtnkshl17@gmail.com>
2025-10-25 13:02:26 -0400

    Interface Communication Problem

---
+++ src/InterfacesCommunicationProblemChetan/SocialMedia.java
@@ -0,0 +1,11 @@
+package InterfacesCommunicationProblemChetan;
+
+public interface SocialMedia extends CommunicationChannels {
+
+    void friendRequest();
+    void textPost();
+    void like();
+    void dislike();
+    void comments();
+
+}
