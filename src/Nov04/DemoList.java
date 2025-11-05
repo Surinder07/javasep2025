2cc0c9b0849e04d16c1640298dbeea828cc0801e
IM <indrani.mukherjee1808@gmail.com>
2025-11-04 22:05:06 -0500

    List

--- src/Nov04/DemoList.java
+++ src/Nov04/DemoList.java
@@ -1,12 +1,13 @@
 package Nov04;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class DemoList {
-    public static void main (String[] args)
-    {
+    public static void main(String[] args) {
 
-        ArrayList<Integer> List= new ArrayList();
+        ArrayList<Integer> List = new ArrayList();
         List.add(10);
         List.add(20);
         List.add(30);
@@ -14,11 +15,19 @@ public class DemoList {
         List.add(50);
         List.add(3455);
         System.out.println(List);
-        List.add(6,1256);
+        List.add(6, 1256);
         System.out.println(List.get(0));
+
+
+        int max = List.get(0);
+
+        for (int num : List) {
+            if (num > max) {
+                max = num;
+            }
+
+            System.out.println("The maximun number is :" + max);
+        }
     }
-
-
-
-
 }
+
