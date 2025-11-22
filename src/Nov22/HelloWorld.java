4dd0094235ec5d5dbf7069f5c6afb7228acf0c1f
IM <indrani.mukherjee1808@gmail.com>
2025-11-22 13:48:37 -0500

    Test

--- src/Nov22/HelloWorld.java
+++ src/Nov22/HelloWorld.java
@@ -2,19 +2,40 @@ package Nov22;
 
 public class HelloWorld {
 
-    public static void main (String[] args)
-    {
+    public static void main(String[] args) {
        /* String a = "Hello";
         a.concat("World");
         System.out.println(a);*/
       /*  String Str1 = "Hello";
         String Str2 = new String("Hello");
         System.out.println(Str1.equals(Str2));*/
-
+/*
         int x = 5;
         int y= 2;
-        System.out.println(x/y);
+        System.out.println(x/y);*/
 
+/*
+        int x = 5;
+        x++;
+        int y = x++;
+        System.out.println(x + y);*/
 
+       /* int[] numbers = {1, 2, 3, 4, 5};
+        int sum = 0;
+        for (int num : numbers) {
+            sum += num;
+        }*/
+
+      /*  String text = "Hello, World!";
+        System.out.println(text.substring(0, 5));*/
+
+        int x = 10;
+        if (x > 5) {
+            System.out.println("A");
+        } else if (x > 7) {
+            System.out.println("B");
+        } else {
+            System.out.println("C");
+        }
     }
 }
